e43bb1dfb6d3614906ff483d018ea741a741f3db
Mowmaster <mowmaster@outlook.com>
2022-06-09 14:01:03 -0500

    setup forge fresh

---
+++ src/main/java/com/mowmaster/effectscrolls/EffectScrollsUtils/References.java
@@ -0,0 +1,6 @@
+package com.mowmaster.effectscrolls.EffectScrollsUtils;
+
+public class References {
+    public static final String MODID = "effectscrolls";
+    public static final String MODNAME = "Effect Scrolls";
+}
